4d6fff2d4e73361b106a69ced57bb9097afc4be3
Salisbury <104432928+SalisburyDev@users.noreply.github.com>
2022-05-17 09:10:29 +0700

    Update SoupListener.java

--- src/main/java/dev/salisbury/soup/SoupListener.java
+++ src/main/java/dev/salisbury/soup/SoupListener.java
@@ -10,7 +10,7 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class SoupListener extends JavaPlugin implements Listener {
+public class SoupListener extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
