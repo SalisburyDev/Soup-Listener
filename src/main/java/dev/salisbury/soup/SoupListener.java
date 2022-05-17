abb84cdccd8708ac9619c96290013824b4733cc4
Salisbury <104432928+SalisburyDev@users.noreply.github.com>
2022-05-17 09:09:23 +0700

    Add files via upload

---
+++ src/main/java/dev/salisbury/soup/SoupListener.java
@@ -0,0 +1,37 @@
+package dev.salisbury.soup;
+
+import com.google.common.base.Objects;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SoupListener extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onInteract(PlayerInteractEvent e) {
+        Player p = e.getPlayer();
+        if (!e.getAction().name().contains("RIGHT_CLICK")) { return; }
+        if (Objects.equal(e.getMaterial(), Material.MUSHROOM_SOUP)) {
+            if (p.getHealth() == p.getMaxHealth()) { return; }
+            e.setCancelled(true);
+
+            double HEAL_AMOUNT = 3.5D * 2.5D;
+            double newHealth = p.getHealth() + HEAL_AMOUNT;
+
+            p.setHealth(Math.min(newHealth, p.getMaxHealth()));
+            p.setItemInHand(new ItemStack(Material.BOWL));
+            p.updateInventory();
+        }
+    }
+
+}
